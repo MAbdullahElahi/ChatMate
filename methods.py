37e86b7c4e91e4d939eb22b62ef35f617d200828
M.Abdullah Elahi <100643084+MAbdullahElahi@users.noreply.github.com>
2023-07-22 00:20:20 +0500

    Update methods.py

--- methods.py
+++ methods.py
@@ -9,9 +9,9 @@ Date Ended June _, 2023
 
 
 # Importing Required Files
-from os import system, path, makedirs
-from customtkinter import CTk, CTkScrollableFrame, CTkFrame, CTkEntry, CTkButton, CTkLabel
-from tkinter import TOP, LEFT, BOTTOM, X, END, messagebox, Menu, filedialog
+from os import path, makedirs
+from customtkinter import CTk, CTkScrollableFrame, CTkFrame, CTkEntry, CTkButton, CTkLabel, set_appearance_mode
+from tkinter import LEFT, BOTTOM, X, BOTH, END, messagebox, Menu, filedialog
 import openai
 from json import dump, load
 from datetime import datetime, date
@@ -23,18 +23,14 @@ CHATMATE_DATA_FILE_PATH:str = f"{home}/Documents/ChatMate/chats"
 if not path.exists(CHATMATE_DATA_FILE_PATH):
     makedirs(CHATMATE_DATA_FILE_PATH)
 
-# Application Dimensions
-app_width = 1000
-app_height = 850
-
 # Font, & Colours
 text_font = ("Arial", 16)
-query_font = ("Arial", 18, "bold")
-datetime_font = ("Arial", 20, "bold")
 highlighted_text_font = ("Arial", 20)
 placeholder_color = ("#F5F5F5", "#2A2A2A")
-text_frame_bg = ("#F5F5F5", "#2A2A2A")
-chat_box_bg = ("#DEDEDE", "#3A3A3A")
+parent_frame_bg = ("#DEDEDE", "#3A3A3A")
+user_chat_frame_bg = ("#fff4d1", "#b58b02")
+response_chat_frame_bg = ("#c9c9c9", "#595858")
+
 
 # Getting Date Time
 time = datetime.now()
@@ -47,6 +43,9 @@ day = dates.day
 
 # Access the api key
 openai.api_key = "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"  
+
+# Apperance Mode Settings
+set_appearance_mode("system")
 
 # Creating Scrollable Frames for chatbox
 class ScrollFrames(CTkScrollableFrame):
@@ -61,25 +60,29 @@ class App(CTk):
         super().__init__()
 
         # Window Properties
-        self.geometry(f'{app_width}x{app_height}')
-        self.title('ChatMate')
+        self.width = self.winfo_screenwidth() - 1300
+        self.height = self.winfo_screenheight() - 200
+        self.geometry(f'{self.width}x{self.height}')
         self.resizable(False, False)
+        self.title('ChatMate')
+
+        
 
         # Create a chatbox frame to store chats
-        self.chatBoxFrame = ScrollFrames(self, width=app_width, corner_radius=8, height=app_height - 150, fg_color=chat_box_bg)
-        self.chatBoxFrame.pack(padx=10, pady=10)
+        self.chatBoxFrame = ScrollFrames(self, width=self.width, corner_radius=8, height=self.height - 150, fg_color=parent_frame_bg)
+        self.chatBoxFrame.pack(padx=10, pady=10, fill=BOTH, expand=True, anchor="center")
 
         #  Create a inputing and submiting frame, Frame/input & submit btn
-        self.frame = CTkFrame(self, corner_radius=0)
+        self.frame = CTkFrame(self, corner_radius=8, fg_color=parent_frame_bg)
 
-        self.input = CTkEntry(self.frame, font=text_font, corner_radius=10, width=app_width - 150, placeholder_text="Type your query (Press Return Key to submit it)")
-        self.input.pack(side=LEFT, padx=20, pady=20, ipady=10)
+        self.input = CTkEntry(self.frame, font=text_font, corner_radius=10, width=self.width - 150, placeholder_text="Type your query (Press Return Key to submit it)")
+        self.input.pack(side=LEFT, padx=20, pady=20, ipady=10, fill=X, expand=True)
 
         self.sbumit_btn = CTkButton(self.frame, text="➤", fg_color="#fcc203", font=highlighted_text_font, width=50,  height=50, hover=None, command=lambda: self.generate(input.get()))
 
-        self.sbumit_btn.pack(side=LEFT, padx=20, pady=20)
+        self.sbumit_btn.pack(side=LEFT, padx=20, pady=20, fill=X)
 
-        self.frame.pack(side=BOTTOM, fill=X)
+        self.frame.pack(side=BOTTOM, padx=10, pady=10, fill=X, expand=True, anchor="s")
 
         # Load the saved chats
         self.load_chat()
@@ -125,32 +128,36 @@ class App(CTk):
                     presence_penalty=0
                 )['choices'][0]['text']
 
-                date_time = f"{hour}:{minute}, {month}/{day}/{year}\n"
-
-                self.chat_frame(dt=date_time, query=query, resp=response)
+                self.chat_frame(query=query, resp=response)
 
                 self.input.delete(0, END)  # Clear the text field
 
-                self.save_chat(date_time, query, response)
+                self.save_chat(query, response)
 
         except Exception as e:
             messagebox.showerror("Error", f"{e}\n")
 
  
     # Methods: Create Chat Frame,
-    def chat_frame(self, dt, query, resp):
-        self.childFrame = CTkFrame(self.chatBoxFrame, fg_color=text_frame_bg, width=app_width, corner_radius=8)
-        self.datetime_label = CTkLabel(self.childFrame, text=dt, font=datetime_font)
-        self.datetime_label.pack(anchor="w", padx=20, pady=10)
-        self.query_text = CTkLabel(self.childFrame, text=f"Query: {query}", font=query_font, wraplength=900,
-                                   justify=LEFT)
-        self.query_text.pack(anchor="w", padx=10, pady=10)
-        self.query_text = CTkLabel(self.childFrame, text=f"Answer: {resp}", font=text_font, wraplength=900,
-                                   justify=LEFT)
-        self.query_text.pack(anchor="w", padx=10, pady=10)
-        self.childFrame.pack(fill=X, pady=10, padx=10)  # Pack frame
+    def chat_frame(self, query, resp):
+        
+        # User Chatting Messages
+        self.user = CTkFrame(self.chatBoxFrame, fg_color=user_chat_frame_bg, width=self.width-150, corner_radius=8)
 
+        self.query = CTkLabel(self.user, text=query, font=text_font, wraplength=self.width-200, justify=LEFT)
+        self.query.pack(padx=5, pady=5, anchor="e")
+        
+        self.user.pack(anchor="e", padx=10, pady=10)  # Pack frame
+        
+        # AI Chatting Messages/Responses
+        self.response = CTkFrame(self.chatBoxFrame, fg_color=response_chat_frame_bg, width=self.width-150, corner_radius=8)
+
+        self.resp = CTkLabel(self.response, text=resp, font=text_font, wraplength=self.width-200, justify=LEFT)
+        self.resp.pack(padx=5, pady=5, anchor="w")
+        
+        self.response.pack(anchor="w", padx=10, pady=10)  # Pack frame
  
+
     # Methods: KeyBoard Events,
     def key_shortcuts(self, event, input: str = ""):
         if event.keysym == "Return" or event.keysym == "KP_Enter":
@@ -158,7 +165,7 @@ class App(CTk):
 
 
     # Methods: Save Chats,
-    def save_chat(self, dt, query, response):
+    def save_chat(self, query, response):
         try:
             chat_file = path.join(CHATMATE_DATA_FILE_PATH, "chat.json")
             with open(chat_file, "r", encoding='utf-8') as f:
@@ -171,7 +178,6 @@ class App(CTk):
 
         # Add new chat data
         chat_data = {
-            "Date-Time": dt,
             "Query": query + "\n",
             "Response": response
         }
@@ -189,18 +195,7 @@ class App(CTk):
             with open(chat_file, "r", encoding='utf-8') as f:
                 data = load(f)
             for item in data:
-                self.childFrame = CTkFrame(self.chatBoxFrame, fg_color=text_frame_bg, width=app_width, corner_radius=16)
-
-                self.datetime_label = CTkLabel(self.childFrame, text=item["Date-Time"], font=datetime_font)
-                self.datetime_label.pack(anchor="w", padx=20, pady=10)
-
-                self.query_text = CTkLabel(self.childFrame, text=f'Query: {item["Query"]}', font=query_font, wraplength=900, justify=LEFT)
-                self.query_text.pack(anchor="w", padx=10, pady=10)
-
-                self.query_text = CTkLabel(self.childFrame, text=f'Answer: {item["Response"]}', font=text_font, wraplength=900, justify=LEFT)
-                self.query_text.pack(anchor="w", padx=10, pady=10)
-
-                self.childFrame.pack(fill=X, pady=10, padx=10)  # Pack frame
+                self.chat_frame(query=item["Query"], resp=item["Response"])
         except:
             data = []
 
@@ -230,6 +225,7 @@ class App(CTk):
         except:
             messagebox.showinfo("Export Chat Info", "There is no DATA provided in the JSON file to be exported")
 
+
     # Methods: Delete Chat if required,
     def delete_chat(self, event=None):
 
@@ -245,10 +241,12 @@ class App(CTk):
 
             messagebox.showinfo("Information!", "All chats are cleared!")
 
+
     # Methods: Open Documents,
     def open_docs(self, event=None):
         print("Hello world, Opening Docs!")
 
+    
     # Methods: Help Functions,
     def help(self, event=None):
         pass
